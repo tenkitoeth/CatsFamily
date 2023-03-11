5fa6176f889ac867b962199faa8bb836da4fcba4
VitaleMarvit <marianovitale018@gmail.com>
2023-03-11 20:54:50 -0300

    arreglos

--- src/ui-labs/Section-Nft/index.js
+++ src/ui-labs/Section-Nft/index.js
@@ -4,7 +4,9 @@ import "./index.css"
 function SectionNftLabs() {
     return (
         <section id="nfts" className="container-section-nfts-labs">
+
             <h1 className="h1-section-nfts-labs">¿Qué es un NFT?</h1>
+            
             <p className="p-section-nfts-labs" data-aos="zoom-in-up">Un NFT es una forma única de representación digital de un activo, lo que significa que cada NFT es único e indivisible (aunque a día de hoy ya se pueden fragmentar NFTs). Los NFTs se suelen utilizar para representar activos digitales como obras de arte, productos coleccionables virtuales y otros tipos de contenido digital. Además, por lo general, son activos ilíquidos y de mucho riesgo, debido a la alta especulación que hay en torno a ello.</p>
         </section> 
     )
