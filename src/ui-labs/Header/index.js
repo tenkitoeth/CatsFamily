dd8a182fb19aa93b1c44697191cfba78140a547c
VitaleMarvit <marianovitale018@gmail.com>
2023-03-12 12:46:12 -0300

    m1

--- src/ui-labs/Header/index.js
+++ src/ui-labs/Header/index.js
@@ -6,7 +6,7 @@ function HeaderLabs() {
     return (
         <section id="header" className="container-header-labs">
 
-            <video className="video-fondo-labs" autoplay loop muted defaultMuted playsinline preload="auto" >
+            <video className="video-fondo-labs" autoplay="autoplay" loop="loop" muted defaultMuted playsinline preload="auto" >
                 <source src={videoPortadaLabs} type="video/mp4" />
             </video>
 
