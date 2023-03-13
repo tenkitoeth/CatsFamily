b2864bdf8e84498e9546766075fecc889bdcdce3
VitaleMarvit <marianovitale018@gmail.com>
2023-03-13 09:59:19 -0300

    deploy root 10

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { Routes, Route, HashRouter, } from "react-router-dom";
 import { Labs } from "./routes/AppUILabs";
 import { Cats } from "./routes/AppUI";
 
 
 function App(){
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/555Labs" element={<Labs />}></Route>
-                <Route path="/Cats/" element={<Cats />}></Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Labs />}></Route>
+                    <Route path="/Cats" element={<Cats />}></Route>
+                </Routes>
+            </HashRouter>
+        </>
     )
 }
 
