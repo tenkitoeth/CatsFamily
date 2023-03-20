730cb94d957cce58d2acc5d4c71d9a354772b54a
VitaleMarvit <marianovitale018@gmail.com>
2023-03-20 18:35:02 -0300

    Actualizacion pagina unica

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route, HashRouter, } from "react-router-dom";
-import { Labs } from "./routes/AppUILabs";
+// import { Labs } from "./routes/AppUILabs";
 import { Cats } from "./routes/AppUI";
 
 
 function App(){
     return (
         <Routes>
-            <Route path="/" element={<Labs />}></Route>
-            <Route path="/Cats" element={<Cats />}></Route>
+            <Route path="/" element={<Cats />}></Route>
+            {/* <Route path="/Cats" element={<Cats />}></Route> */}
         </Routes>
     )
 }
