11baf0fa721ff9b5c870a95e4a69e292baa39ad6
VitaleMarvit <marianovitale018@gmail.com>
2023-06-26 08:27:06 -0300

    nuevos cambios video

--- src/ui-cats/Header/Header-Video/index.js
+++ src/ui-cats/Header/Header-Video/index.js
@@ -5,7 +5,7 @@ import videoPortada from "./fondo_portada.mp4"
 function VideoFondo(){
 
     return (
-        <video className="video-fondo" autoPlay="autoplay" loop={true} muted={muted} defaultmuted="true" playsInline={true} disablePictureInPicture={true} >
+        <video className="video-fondo" autoPlay="autoplay" loop={true} muted={true} defaultmuted="true" playsInline={true} disablePictureInPicture={true} >
             <source src={videoPortada} type="video/mp4" />
         </video>
     )
